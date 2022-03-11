2864cfc54db59b004252078601c8d0c67db89fe4
alibingoll <aliekber.bngl@gmail.com>
2022-03-11 09:02:45 +0300

    Todo2

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoComponent{
 
-  constructor() { }
+  constructor() { } 
   private name: string = "Sadık"
   model: Model = new Model();
  
